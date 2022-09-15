661a8d11511e93475edd154926ff62efb98bd286
chikqje <jessica.aulia11@ui.ac.id>
2022-09-15 13:55:32 +0700

    Lab 2 PBP 2022

--- wishlist/views.py
+++ wishlist/views.py
@@ -15,10 +15,11 @@ def show_json(request):
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml") 
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Jessica Aulia'
 }
-    
